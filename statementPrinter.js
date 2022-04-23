c6fcaec9dda76dbb915d6c3b6c9c25c0b90ad24d
Paul <pkilgarriff@gmail.com>
2022-04-23 15:15:09 +0100

    statement method now generates transaction strings for single transaction

--- statementPrinter.js
+++ statementPrinter.js
@@ -1,7 +1,38 @@
 class StatementPrinter {
   static statement(transactions) {
-    const statementString = "date || credit || debit || balance";
+    let cumulativeBalance = 0;
+    let statementString = "date || credit || debit || balance";
+    if (typeof transactions === "undefined" || transactions.length === 0)
+      return statementString;
+    transactions.forEach((transaction) => {
+      cumulativeBalance += transaction.amount;
+      let creditDebitColumns;
+      if (transaction.type === "credit") {
+        creditDebitColumns = `${this.currencyFormatter(transaction.amount)} ||`;
+      } else {
+        creditDebitColumns = `|| ${this.currencyFormatter(transaction.amount)}`;
+      }
+      let transactionString = `\n${this.dateFormatter(
+        transaction.date
+      )} || ${creditDebitColumns} || ${this.currencyFormatter(
+        cumulativeBalance
+      )}`;
+      statementString = statementString.concat(transactionString);
+    });
     return statementString;
+  }
+
+  static dateFormatter(date) {
+    return date.toLocaleString().split(",")[0];
+  }
+
+  static currencyFormatter(amount) {
+    const decimalPlaces = 2;
+    return Number(
+      Math.round(parseFloat(amount + "e" + decimalPlaces)) +
+        "e-" +
+        decimalPlaces
+    ).toFixed(decimalPlaces);
   }
 }
 
--- statementPrinter.test.js
+++ statementPrinter.test.js
@@ -6,4 +6,28 @@ describe("StatementPrinter", () => {
       "date || credit || debit || balance"
     );
   });
+  test("correct statement when given an array of one credit transaction", () => {
+    transactions = [
+      {
+        date: new Date("2022-05-14T00:00:00.000Z"),
+        amount: 15,
+        type: "credit",
+      },
+    ];
+    expect(StatementPrinter.statement(transactions)).toEqual(
+      "date || credit || debit || balance\n14/05/2022 || 15.00 || || 15.00"
+    );
+  });
+  test("correct statement when given an array of one debit transaction", () => {
+    transactions = [
+      {
+        date: new Date("2022-05-14T00:00:00.000Z"),
+        amount: 15,
+        type: "debit",
+      },
+    ];
+    expect(StatementPrinter.statement(transactions)).toEqual(
+      "date || credit || debit || balance\n14/05/2022 || || 15.00 || 15.00"
+    );
+  });
 });
